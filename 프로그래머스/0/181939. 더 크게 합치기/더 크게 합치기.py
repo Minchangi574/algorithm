459095d090d1977dc59e3b8c6fdbe3fc7771b735
Minchangi574 <cksrl0956@gmail.com>
2025-07-08 13:48:57 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 9.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -0,0 +1,10 @@
+def solution(a, b):
+    a_num=str(a)+str(b)
+    b_num=str(b)+str(a)
+    int(a_num)
+    int(b_num)
+    if a_num>b_num:
+        answer=int(a_num)
+    else:
+        answer=int(b_num)
+    return answer
